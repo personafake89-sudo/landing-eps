468fd2f52f9ba562efd6f921d84e1671816507bb
personafake <personafake89@gmail.com>
2026-05-30 12:16:37 -0500

    fix: corregir BASE_URL a epsemaq.epsagua.com

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.epsagua.com",
+      url: "https://epsemaq.epsagua.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
